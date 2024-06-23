a51fd6f5aa48c9a7c7a381eb9ad06e5bca5d998f
mogimogiko <155148504+mogimogiko@users.noreply.github.com>
2024-06-23 09:41:53 +0900

    課題を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 配列
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 出力
+    print_r($personal_data);
+    ?>
+  </p>
+</body>
+</html>
